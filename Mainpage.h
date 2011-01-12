ea03d46c8d44fc7e62078be403c46d0e2b960ecf
Michal Charmas <michal@charmas.pl>
2011-01-12 21:27:19 +0100

    added documentation main page

---
+++ Mainpage.h
@@ -0,0 +1,14 @@
+/** @mainpage About
+*
+* @authors Michał Charmas
+*
+* @section intro Introduction
+* This software can be used as a TouchTracer in optical touch screens.
+* Supports TUIO interface for sending touch information.
+* <hr>
+* @section notes Release notes
+* Supports a lot of things...
+* <hr>
+* @todo Finish calibration module.
+*
+*/
